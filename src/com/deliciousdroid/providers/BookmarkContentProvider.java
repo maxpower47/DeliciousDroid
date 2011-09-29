7d11748e00eb0d7a667e1adfa8cacc289e6b2231
maxpower47 <matthewrschmidt@gmail.com>
2011-09-29 12:54:29 -0400

    Reset db version number.

--- src/com/deliciousdroid/providers/BookmarkContentProvider.java
+++ src/com/deliciousdroid/providers/BookmarkContentProvider.java
@@ -63,7 +63,7 @@ public class BookmarkContentProvider extends ContentProvider {
 	private SQLiteDatabase db;
 	private DatabaseHelper dbHelper;
 	private static final String DATABASE_NAME = "DeliciousBookmarks.db";
-	private static final int DATABASE_VERSION = 21;
+	private static final int DATABASE_VERSION = 22;
 	private static final String BOOKMARK_TABLE_NAME = "bookmark";
 	private static final String TAG_TABLE_NAME = "tag";
 	private static final String BUNDLE_TABLE_NAME = "bundle";
