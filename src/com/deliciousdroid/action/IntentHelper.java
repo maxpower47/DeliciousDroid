ffc4560bc684fac54e34e631e72fa6bb8ee79fef
Matthew Schmidt <matthewrschmidt@gmail.com>
2012-09-10 14:55:10 -0400

    Change copyright notices.

--- src/com/deliciousdroid/action/IntentHelper.java
+++ src/com/deliciousdroid/action/IntentHelper.java
@@ -1,3 +1,24 @@
+/*
+ * DeliciousDroid - http://code.google.com/p/DeliciousDroid/
+ *
+ * Copyright (C) 2010 Matt Schmidt
+ *
+ * DeliciousDroid is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published
+ * by the Free Software Foundation; either version 3 of the License,
+ * or (at your option) any later version.
+ *
+ * DeliciousDroid is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with DeliciousDroid; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307
+ * USA
+ */
+
 package com.deliciousdroid.action;
 
 import java.io.UnsupportedEncodingException;
@@ -64,7 +85,7 @@ public class IntentHelper {
 		Intent viewBookmark = new Intent(context, ViewBookmark.class);
 		viewBookmark.setAction(Intent.ACTION_VIEW);
 		viewBookmark.addCategory(Intent.CATEGORY_DEFAULT);
-		viewBookmark.putExtra("com.pindroid.BookmarkViewType", type);
+		viewBookmark.putExtra("com.deliciousdroid.BookmarkViewType", type);
 		Uri.Builder data = new Uri.Builder();
 		data.scheme(Constants.CONTENT_SCHEME);
 		data.encodedAuthority(account + "@" + BookmarkContentProvider.AUTHORITY);
